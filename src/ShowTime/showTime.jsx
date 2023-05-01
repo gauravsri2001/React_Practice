4173fc729edcd7b9d7cef6eb8072d411091e8e98
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-05-01 11:59:53 +0530

    add

--- src/ShowTime/showTime.jsx
+++ src/ShowTime/showTime.jsx
@@ -17,18 +17,3 @@ const ShowTime = () => {
 };
 export default ShowTime;
 
-// import React, { useState } from "react";
-// const UseSate_P = () => {
-// const state = useState();
-// const[count, setCount] = useState(0) //here count is the initial state and setCount is the final state
-//     const Incnum = () => {
-//         setCount(count+1)   
-//     }
-//     return (
-//         <>
-//         <h1>{count}</h1>
-//         <button onClick={Incnum}>Click me</button>
-//         </>
-//     ); 
-// };
-// export default UseSate_P;
