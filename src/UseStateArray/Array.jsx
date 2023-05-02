aa8caaf01d9f486cacf4445a41e69db36aa18c63
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-05-02 12:57:16 +0530

    handling Array

--- src/UseStateArray/Array.jsx
+++ src/UseStateArray/Array.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Array = () => {
+
+    const data = [
+        {
+            id:1 , Name: "Gaurav", Age: 21
+        },
+        {
+            id:2 , Name: "Rahul", Age: 31
+        },
+        {
+            id:3 , Name: "Raghav", Age: 27
+        }
+    ]
+
+    //using usestate for clear button
+    const[myArray,setmyArray] = useState(data)
+
+    const clearArray = () =>{
+        setmyArray([]);
+    }
     return(
         <>
-<h1>bbbb</h1>
+        {
+            myArray.map((elements) => <h1 key={elements.id}> Name: {elements.Name}, Age: {elements.Age}</h1>)
+        }
+        
+        <button onClick={clearArray}>Clear</button>
+
         </>
     );
 };
