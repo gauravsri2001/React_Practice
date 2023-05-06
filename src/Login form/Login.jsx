7cb21dd7d0155471cd01b0b1cfec7b329439b7be
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-05-06 22:40:12 +0530

    add

--- src/Login form/Login.jsx
+++ src/Login form/Login.jsx
@@ -1,38 +1,50 @@
 
 import React, { useState } from 'react';
 
-function Login() {
-  const [username, setUsername] = useState('');
+const Login = () => {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newEntry, setNewEntry] = useState([]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Username:', username);
-    console.log('Password:', password);
-    // Perform login authentication
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log('Username:', username);
+  //   console.log('Password:', password);
+  //   // Perform login authentication
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+    <form action=''>
+
+    <div>
+    <label htmlFor='email'> Email </label>
+    <input 
+    type="text" 
+    name='email' 
+    id='email' 
+    autoComplete='off' 
+    value={email}
+      onChange={(e) =>setEmail(e.target.value)}
+    />
+    </div>
+      
+      <div>
+      <label htmlFor='password'>Password </label>
+        
+        <input 
+        type="password" 
+        name='password' 
+        id='password' 
+        autoComplete='off'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
+      
+      </div>
+      
       <button type="submit">Log in</button>
     </form>
   );
-}
+  };
+
 
 export default Login;
