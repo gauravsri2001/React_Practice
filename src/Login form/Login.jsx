854e988459512913fc304dca888438239b13fc5e
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-05-06 23:01:02 +0530

    update

--- src/Login form/Login.jsx
+++ src/Login form/Login.jsx
@@ -4,7 +4,15 @@ import React, { useState } from 'react';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [newEntry, setNewEntry] = useState([]);
+  const [allEntry, setAllEntry] = useState([]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const newEntry = {email: email, password: password};
+    setAllEntry([...allEntry, newEntry]);
+    console.log(allEntry)
+  }
 
   // const handleSubmit = (event) => {
   //   event.preventDefault();
@@ -13,36 +21,52 @@ const Login = () => {
   //   // Perform login authentication
 
   return (
-    <form action=''>
+    <>
+<form action='' onSubmit={submitForm}>
 
-    <div>
-    <label htmlFor='email'> Email </label>
+<div>
+<label htmlFor='email'> Email </label>
+<input 
+type="text" 
+name='email' 
+id='email' 
+autoComplete='off' 
+value={email}
+  onChange={(e) =>setEmail(e.target.value)}
+/>
+</div>
+  
+  <div>
+  <label htmlFor='password'>Password </label>
+    
     <input 
-    type="text" 
-    name='email' 
-    id='email' 
-    autoComplete='off' 
-    value={email}
-      onChange={(e) =>setEmail(e.target.value)}
+    type="password" 
+    name='password' 
+    id='password' 
+    autoComplete='off'
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
     />
-    </div>
-      
-      <div>
-      <label htmlFor='password'>Password </label>
-        
-        <input 
-        type="password" 
-        name='password' 
-        id='password' 
-        autoComplete='off'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        />
-      
-      </div>
-      
-      <button type="submit">Log in</button>
-    </form>
+  
+  </div>
+  
+  <button type="submit">Log in</button>
+</form>
+
+<div>
+  {
+    allEntry.map((curElem) =>{
+return(
+  <div> 
+  <p>{curElem.email}</p>
+  <p>{curElem.password}</p>
+  </div>
+)
+    })
+  }
+</div>
+    </>
+    
   );
   };
 
